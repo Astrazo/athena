c80df1ba67fb3b4d652785d7f3a55cbc0b6ad810
Joshua Richardson <jric0040@student.monash.edu>
2025-05-31 18:15:32 +1000

    Initial commit.

---
+++ story/firebase.js
@@ -0,0 +1,26 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzgDI28nu9RGm32mJH-1tvkZCQOdEjdrk",
+  authDomain: "athena-astrazo.firebaseapp.com",
+  projectId: "athena-astrazo",
+  storageBucket: "athena-astrazo.firebasestorage.app",
+  messagingSenderId: "879430407218",
+  appId: "1:879430407218:web:6c514226b384a7857b182b"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
+
+window.createNewRoom = async function(name) {
+  const db = firebase.firestore();  
+  const docRef = await db.collection("rooms").add({
+    name: name,
+    created: firebase.firestore.FieldValue.serverTimestamp(),
+    status: "waiting",
+    players: []
+  });
+
+  console.log("Room created with ID", docRef.id);
+  return docRef.id;
+};
+
+
+
