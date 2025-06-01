cb0779682ab90b2ea9d7e3b406ff5f525785024a
Joshua Richardson <jric0040@student.monash.edu>
2025-06-01 14:37:18 +1000

    Setup nicknames, setup player tracking in rooms when you create it

--- story/scripts/firebase.js
+++ story/scripts/firebase.js
@@ -22,26 +22,74 @@ window.createRoom = async function(name) {
     return docRef.id;
 };
 
+// Add nickname to the users collection
+window.createUser = async function (userName, roomName, roomId) {
+    const docRef = await db.collection("users").add({
+        userName: userName,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        roomName: roomName,
+        roomId: roomId
+    })
+
+    console.log("User created with ID", docRef.id);
+    return docRef.id;
+}
+
+window.addUserToRoom = async function (userId, userName, roomId) {
+    const roomRef = db.collection("rooms").doc(roomId)
+    const playerEntry = {
+        id: userId,
+        name: userName
+    }
+    await roomRef.update({
+        players: firebase.firestore.FieldValue.arrayUnion(playerEntry)
+    });
+    console.log("Added player", userName, "to", roomRef.roomName, ".")
+}
+
 // Load all current rooms by status
-window.loadRooms = async function (status) {
-    const rooms = [];
+window.loadRooms = async function (status, roomId) {
     try {
-        const snapshot = await db.collection("rooms")
-        .where("status", "==", status)
-        .orderBy("created", "desc")
-        .get();
-        snapshot.forEach(doc => {
-            rooms.push({
-                id: doc.id,
-                name: doc.data().name
-            })
-        })
-        return rooms
+        if (roomId) {
+            const snapshot = await db.collection("rooms")
+                .where("__name__", "==", roomId)
+                .get();
+
+            const doc = snapshot.docs[0];
+            if (!doc) return null; // room not found
+
+            return {
+                roomName: doc.data().name,
+                roomPlayers: doc.data().players
+            };
+        }
+        else if (status) {
+            const snapshot = await db.collection("rooms")
+                .where("status", "==", status)
+                .orderBy("created", "desc")
+                .get();
+
+            const rooms = [];
+            snapshot.forEach(doc => {
+                rooms.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    players: doc.data().players
+                });
+            });
+            return rooms;
+        }
+
+        return [];
+
     } catch (err) {
         console.error("Error loading rooms:", err);
         return [];
     }
-}
+};
+
+
+
 
 
 
