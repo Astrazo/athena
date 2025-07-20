2f7be4699b5e33bf3e91e1d62018017855162d18
Joshua Richardson <jric0040@student.monash.edu>
2025-07-20 13:13:26 +1000

    Players are now alerted when their enabled value is set, and by whom it was set by using the enabler-map. - Players are no longer told to wait in the situation room unless both of their tasks are done (previously it would do this if they somehow did their enabled task first) - Objective div is now updated with every update of the screen, enabeling more precice objective direction

--- src/js/objectives.js
+++ src/js/objectives.js
@@ -31,6 +31,12 @@ export function setCurrentObjective(roomName, roomData = null, currentRole = nul
         const enabledComplete = roomData["roomPlayers"][localUserId]["enabledComplete"];
         const enabledPossible = roomData["roomPlayers"][localUserId]["enabledValue"] != null;
 
+        // Check if both tasks are complete - if so, player should return to situation room
+        if (enabledComplete && enablerComplete) {
+            updateObjectiveDisplay('Objective', "Return to situation room and wait.");
+            return;
+        }
+
         // Import the function from main.js
         import('./main.js').then(({ isRoomActionableRightNow }) => {
             const [isActionable] = isRoomActionableRightNow(
@@ -38,14 +44,14 @@ export function setCurrentObjective(roomName, roomData = null, currentRole = nul
             );
 
             if (isActionable) {
-                updateObjectiveDisplay('Objective', "Choose an action to complete this task");
+                updateObjectiveDisplay('Objective', "Choose an action to complete this task.");
             } else {
-                updateObjectiveDisplay('Objective', "Explore other rooms");
+                updateObjectiveDisplay('Objective', "Explore other rooms.");
             }
         });
     } else {
         // Fallback for when we don't have room data
-        updateObjectiveDisplay('Objective', "Explore other rooms");
+        updateObjectiveDisplay('Objective', "Explore other rooms.");
     }
 }
 
